f2fffddc58ea9c26a3e08eccce67e35f8ac1da18
wlstjjw <momo37025@naver.com>
2021-07-23 12:04:07 +0900

    section3 글자 왼쪽에서 나오게 수정중

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ scrstSlide1.style.left = - scrstLeft + '%';
 scrstSlide2.style.left = scrstLeft + '%';
 scrollstart.style.height = scrstHeight + 'vh';
 
+const section2 = document.querySelector('#section2');
 const rightSlide = document.querySelector('.right-slide');
 const slideBox = document.querySelector('.slide-box');
 const lightgraySlide = document.querySelector('.lightgray-slide');
@@ -16,12 +17,11 @@ let slideBoxTop = 0;
 slideBox.style.top = slideBoxTop;
 
 // let ligrayRight = 150;
-let dkgrayRight = 215;
+// let dkgrayRight = 215;
 lightgraySlide.style.right = - 140 + '%';
-darkgraySlide.style.right = - dkgrayRight + '%';
+darkgraySlide.style.right = - 215 + '%';
 
 const section3 = document.querySelector('#section3');
-console.log(section3.offsetTop)
 
 addEventListener('scroll', function () {
     const scrolltop = document.documentElement.scrollTop;
@@ -29,10 +29,20 @@ addEventListener('scroll', function () {
     scrstSlide1.style.left = (scrolltop / 7) - scrstLeft + '%';
     scrstSlide2.style.left = - (scrolltop / 7) + scrstLeft + '%';
 
-    height = `calc(${rightSlide.offsetTop}px-250vh)`;
-    console.log(height);
+    const announce = document.querySelector('.pantone-announce p');
 
-    if (scrolltop >= rightSlide.offsetTop && scrolltop < section3.offsetTop) {
+    if (scrolltop >= section2.offsetTop - this.window.innerHeight / 2) {
+        announce.style.top = '50%';
+        announce.style.opacity = 1;
+    }
+
+    if (scrolltop >= announce.offsetTop - this.window.innerHeigth / 2) {
+        const colorvalue = this.document.querySelector('.bold-colorvalue');
+        colorvalue.style.left = 0;
+    }
+
+
+    if (scrolltop >= rightSlide.offsetTop && scrolltop < section3.offsetTop - this.window.innerHeight) {
         slideBox.style.top = scrolltop - rightSlide.offsetTop + 'px';
 
         let ligrayRight = ((scrolltop - rightSlide.offsetTop) / 10) - 140;
@@ -40,7 +50,12 @@ addEventListener('scroll', function () {
 
         if (ligrayRight >= -50) {
             lightgraySlide.style.right = - 50 + '%';
-            darkgraySlide.style.right = (scrolltop - rightSlide.offsetTop) / 10 - dkgrayRight + '%';
+            let dkgrayRight = (scrolltop - rightSlide.offsetTop) / 10 - 215;
+            darkgraySlide.style.right = dkgrayRight + '%';
+
+            if (dkgrayRight >= -70) {
+                darkgraySlide.style.right = -70 + '%';
+            }
         }
     }
 })
