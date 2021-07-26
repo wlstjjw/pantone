de0d91e94199e1eb98eeea7b517acb68b7a5b582
wlstjjw <momo37025@naver.com>
2021-07-26 17:28:16 +0900

    section4 작업중

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const scrollstart = document.querySelector('.scrollstart');
 const scrstSlide1 = document.querySelector('.scrstart-slide1');
 const scrstSlide2 = document.querySelector('.scrstart-slide2');
+
 let scrstLeft = 100;
 let scrstHeight = 40;
+
 scrstSlide1.style.left = - scrstLeft + '%';
 scrstSlide2.style.left = scrstLeft + '%';
 scrollstart.style.height = scrstHeight + 'vh';
@@ -19,7 +21,7 @@ slideBox.style.top = slideBoxTop;
 // let ligrayRight = 150;
 // let dkgrayRight = 215;
 lightgraySlide.style.right = - 140 + '%';
-darkgraySlide.style.right = - 215 + '%';
+darkgraySlide.style.right = - 200 + '%';
 
 const section3 = document.querySelector('#section3');
 
@@ -29,16 +31,17 @@ addEventListener('scroll', function () {
     scrstSlide1.style.left = (scrolltop / 7) - scrstLeft + '%';
     scrstSlide2.style.left = - (scrolltop / 7) + scrstLeft + '%';
 
-    const announce = document.querySelector('.pantone-announce p');
+
 
     if (scrolltop >= section2.offsetTop - this.window.innerHeight / 2) {
+        const announce = document.querySelector('.pantone-announce p');
         announce.style.top = '50%';
         announce.style.opacity = 1;
     }
 
-    if (scrolltop >= announce.offsetTop - this.window.innerHeigth / 2) {
-        const colorvalue = this.document.querySelector('.bold-colorvalue');
-        colorvalue.style.left = 0;
+    if (scrolltop >= section2.offsetTop + this.window.innerHeight / 3) {
+        const colorvalue = this.document.querySelector('.colorvalue');
+        colorvalue.style.left = '0';
     }
 
 
@@ -48,14 +51,36 @@ addEventListener('scroll', function () {
         let ligrayRight = ((scrolltop - rightSlide.offsetTop) / 10) - 140;
         lightgraySlide.style.right = ligrayRight + '%';
 
-        if (ligrayRight >= -50) {
-            lightgraySlide.style.right = - 50 + '%';
-            let dkgrayRight = (scrolltop - rightSlide.offsetTop) / 10 - 215;
+        if (ligrayRight >= 0) {
+            lightgraySlide.style.right = 0 + '%';
+
+            let dkgrayRight = (scrolltop - rightSlide.offsetTop) / 15 - 200;
             darkgraySlide.style.right = dkgrayRight + '%';
 
-            if (dkgrayRight >= -70) {
-                darkgraySlide.style.right = -70 + '%';
+            if (dkgrayRight >= -40) {
+                darkgraySlide.style.right = -40 + '%';
             }
         }
+    }
+
+    if (scrolltop >= section3.offsetTop - 500) {
+        const colorpallet = document.querySelector('.colorpallet');
+        colorpallet.style.left = '0';
+    }
+
+    const colorblockL = document.querySelector('.go-left-block');
+    const colorblockR = document.querySelector('.go-right-block');
+    const fixedblock1 = document.querySelector('.fixed-block1');
+    const fixedblock2 = document.querySelector('.fixed-block2');
+
+    if (scrolltop >= section3.offsetTop + 750 && scrolltop <= section3.offsetTop + 750 + this.window.innerWidth) {
+        colorblockL.style.top = (scrolltop - (section3.offsetTop + 750)) + 'px';
+        colorblockR.style.top = (scrolltop - (section3.offsetTop + 750)) + 450 + 'px';
+
+        colorblockL.style.left = - (scrolltop - (section3.offsetTop + 750)) + 'px';
+        colorblockR.style.right = - (scrolltop - (section3.offsetTop + 750)) + 'px';
+
+        fixedblock1.style.top = (scrolltop - (section3.offsetTop + 750)) + 900 + 'px';
+        fixedblock2.style.top = (scrolltop - (section3.offsetTop + 750)) + 1350 + 'px';
     }
 })
